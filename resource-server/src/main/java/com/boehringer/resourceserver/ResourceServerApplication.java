23a873f85c0996f11268fbce03f3855ab27249f5
Lisandro Medrano <osvaldo.medrano@rappi.com>
2020-08-20 19:56:15 -0300

    Added Benchmark to queries

--- resource-server/src/main/java/com/boehringer/resourceserver/ResourceServerApplication.java
+++ resource-server/src/main/java/com/boehringer/resourceserver/ResourceServerApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableAutoConfiguration
-//@ComponentScan("com.boehringer.resourceserver.model")
 public class ResourceServerApplication {
 
 	public static void main(String[] args) {
